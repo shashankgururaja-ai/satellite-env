7f40ba32b5bc5c05a9f288b5f94755e1b2a77d11
G Shashank <shashankgururaja@gmail.com>
2026-04-12 13:26:29 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,9 +2,8 @@ from openenv_core.env_server import create_app
 from .env import SatelliteEnv
 from .schema import SatelliteAction, SatelliteObservation
 
-# We now pass the environment and models directly without keywords!
 app = create_app(
-    lambda: SatelliteEnv(),
+    SatelliteEnv(),
     SatelliteAction,
     SatelliteObservation,
     env_name="aerospace_satellite_manager"
